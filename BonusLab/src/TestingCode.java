62e2987a11af17ae016e1b6d36c71c42710b2e8f
3010a14 <3010a14@guama.ece.uprm.edu>
2017-06-22 17:50:51 -0400

    iqnirfmgbthj

---
+++ BonusLab/src/TestingCode.java
@@ -0,0 +1,68 @@
+
+
+public class TestingCode {
+	public static void main(){
+		String[] voteses ={"Jorge", "Jorge", "Jorge"};
+		System.out.println(electionWinner(voteses));;
+		
+	}
+
+	static String electionWinner(String[] votes) {
+		int countvotes[]= {0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+
+
+		for(int i=1; i<votes.length; i++){
+			if(!votes[i].equals(" ")){
+				for(int t=i+1; t<votes.length; t++){
+
+					if(votes[i].equals(votes[t]))
+					{
+						countvotes[i]++;
+						votes[t]= " ";
+					}
+
+
+				}
+
+			}
+
+		}
+		int max = countvotes[0];
+		int max2 = 0;
+		int savei =0;
+		int save2i=0;
+		for(int i=1; i<votes.length; i++)
+		{
+			if(i == 0)
+			{
+				max =countvotes[i];
+			}
+			else if(countvotes[i]>max){
+				max =countvotes[i];
+				savei =i;
+			}
+			else if (countvotes[i]==max){
+				if(votes[savei].compareTo(votes[i])> 0){
+					//nothing
+				}
+					
+				else{
+					max =countvotes[i];
+					savei =i;
+				}
+				
+			}
+		}
+		if (max == max2){
+			if(votes[savei].compareTo(votes[save2i])> 0){
+				return votes[savei];
+			}
+			else
+				return votes[save2i];
+			
+		}
+		
+
+		return votes[savei];
+	}
+}
